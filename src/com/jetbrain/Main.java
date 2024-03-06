45ef96036d87aba72ab349a2235224861409b40f
JayChandiramani <jaychandiramani2003@gmail.com>
2024-03-06 16:05:52 -0500

    Initial commit

---
+++ src/com/jetbrain/Main.java
@@ -0,0 +1,17 @@
+package com.jetbrain;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World"); //Plain
+	    System.out.println("Welcome");
+	    System.out.println("Jay Chandiramani");
+	    System.out.println("\t Hello World"); //Tab
+        System.out.println("\"Hello World\""); //Quotes
+	    System.out.println("\\Hello World"); //Slash
+	    System.out.println("Hello\nWorld"); //New Line
+        // This is a new line
+        System.out.println();
+        System.out.println("Hello World\nWelcome\n\"Jay Chandiramani\"");
+    }
+}
